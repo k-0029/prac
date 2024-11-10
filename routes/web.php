9343c5410d4741241f9004f0507299ddedcf9443
k-0029 <koki12131410@icloud.com>
2024-11-10 14:04:31 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
